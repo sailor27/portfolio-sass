3a25d244fb88036886679089adfae144ba57f743
Sailor Winkelman <sailor.winkelman@modop.com>
2019-01-06 14:11:28 -0800

    Add style property of center div based on vph

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(function() {
+  
+  $(document).ready(function(){
+    const vph = window.innerHeight;
+    console.log("height:", vph);
+    $('.center').css({
+      "height": `${vph}px` 
+     })
+  })
+
 	 $('.scroll-down').click (function() {
 		 console.log('🎉');
 		 $('html, body').animate({scrollTop: $(".featured").offset().top }, 'slow');
 		 return false;
-	 });
+   });
+   
 
  });
 
